cab8f698cff0b0912b5aa34be95700c0d387365f
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-02-23 21:48:21 +0530

    Create player set5 5th.c

---
+++ player set5 5th.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+	int k,temp=0,i,j,n;
+	char a[50];
+	scanf("%s",a);
+    scanf("%d",&k);
+	n=strlen(a);
+    for(i=0;i<k;i++)
+    {
+    temp=a[n-1];
+    for(j=n-1;j>=0;j--)
+    {
+	       a[j]=a[j-1];
+
+	 }	    a[0]=temp;
+
+	}
+	for(i=0;i<n;i++)
+{	    printf("%c",a[i]);
+	}
+return 0;
+    }
