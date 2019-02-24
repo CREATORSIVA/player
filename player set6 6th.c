89fd175b1f0505812b2b9087782b781a7aa8c80c
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-02-24 09:47:40 +0530

    Update player set6 6th.c

--- player set6 6th.c
+++ player set6 6th.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include<string.h>
 
 int main()
 {
     char a[50],ch;
-    int i,c=0;
+    int i,c=0,l=0;
     scanf("%s %c",a,&ch);
-    
+    l=strlen(a);
+    if(l<100000)
     for(i=0;a[i]!='\0';i++)
     {
         if(a[i]==ch)
