581adb53ce567d3b24df571b741529781e0f41c8
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-02-27 15:56:04 +0530

    Create player set7 5th.c

---
+++ player set7 5th.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+    int i,j,n,x,c=0,a[40],b[60],k,temp=0;
+    scanf("%d",&n);
+    
+    if(n<=100000)
+    for(i=0;i<n;i++)
+
+{
+    scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+
+{
+    for(j=i+1;j<n;j++)
+    {
+        
+    if(a[j]<a[i])
+    {
+        temp=a[j];
+        a[j]=a[i];
+        a[i]=temp;
+    }
+    }
+}
+for(i=0;i<n;i++)
+{
+   if(a[i]<n)
+   {
+       printf(" %d",a[i]);
+   }
+}
+
+    return 0;
+}
