1d0a0744bd2950db91346e82b9f6675dd971432e
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-03-01 21:24:11 +0530

    Update player set8 4th.c

--- player set8 4th.c
+++ player set8 4th.c
@@ -6,7 +6,10 @@ int main()
 {
     int a,b,i,mul=0;
     scanf("%d%d",&a,&b);
+    if(((a>=1)||(a<=50))&&((b>=1)||(b<=50)))
     mul=pow(a,b);
     printf("%d",mul);
+    else
+    printf("invalid");
     return 0;
 }
