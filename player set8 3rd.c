e094b42242577f4389e7468b72266cc76ee663aa
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-03-01 21:03:11 +0530

    Create player set8 3rd.c

---
+++ player set8 3rd.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,a[50],j=0,t=0,k=0;
+    scanf("%d",&n);
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(k==a[i])
+        {
+        
+            j=i;
+        }
+        
+    }
+   
+    printf("%d",j+1);
+    return 0;
+}
