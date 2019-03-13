8518f25a7fbbb8c6b17ebc3c4c50de3e0d6a38d0
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-03-13 22:11:26 +0530

    Update player set11 5th.c

--- player set11 5th.c
+++ player set11 5th.c
@@ -2,27 +2,27 @@
 
 void main()
 {
-   int a[1000],b[10000],n,temp,i,j;
+   int c[1000],b[10000],n,temp,i,j;
    scanf("%d",&n);
    for(i=0;i<n;i++)
     scanf("%d",&a[i]);
   
    for(i=0;i<n;i++)
-    b[i]=a[i];
+    b[i]=c[i];
   for(i=0;i<n;i++)
         for(j=i;j<n;j++)
      
-          if(a[i]>a[j])
+          if(c[i]>c[j])
           {
-              temp=a[i];
-              a[i]=a[j];
-              a[j]=temp;
+              temp=c[i];
+              c[i]=c[j];
+              c[j]=temp;
             }
     
    for(i=0;i<n;i++)
   for(j=0;j<n;j++)
       
-       if(a[i]==b[j])
+       if(c[i]==b[j])
          printf("%d ",j+1);
      
 
