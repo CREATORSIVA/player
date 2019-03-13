6ffddb538ec712034f12476b3aac36c449bffe0e
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-03-13 19:58:03 +0530

    Update player set9 2nd.c

--- player set9 2nd.c
+++ player set9 2nd.c
@@ -2,12 +2,12 @@
 void main()
 {
 	int n,i,h;
-    int a[10];
+    int b[10];
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
-			scanf("%d",&a[i]);
-	h=a[0];
+			scanf("%d",&b[i]);
+	h=b[0];
 	for(i=0;i<n;i++)
-		h=h&a[i];
+		h=h&b[i];
 	printf("%d",h);
 }
