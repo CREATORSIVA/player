732f0adf5c99ee2d0ec14bc6b070045cacb98151
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-03-13 21:39:27 +0530

    Create player set11 2nd.c

---
+++ player set11 2nd.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    int num,a[100],s=0,i;
+
+    scanf("%d",&num);
+
+    for(i=0;i<num;i++)
+
+    {
+
+        scanf("%d",&a[i]);
+
+    }
+
+    for(i=0;i<num;i++)
+
+    {
+
+        s=s+a[i];
+
+    }
+
+    printf("%d",s);
+
+return 0;
+
+}
