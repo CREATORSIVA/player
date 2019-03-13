220d39a0d72971df4610b92dc5ee19c416035706
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-03-13 20:34:03 +0530

    Update player set9 9th.c

--- player set9 9th.c
+++ player set9 9th.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 void main()
 {
-long int n,r,i,j,p,fact=1,fact1=1;
-scanf("%ld %ld",&n,&r);
-for(i=1;i<=n;i++)
+long int num,r,i,j,p,fact=1,fact1=1;
+scanf("%ld %ld",&num,&r);
+for(i=1;i<=num;i++)
 {
 fact=fact*i;
 }
-for(i=1;i<=n-r;i++)
+for(i=1;i<=num-r;i++)
 {
 fact1=fact1*i;
 }
