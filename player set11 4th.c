90c7d9026463d1cf0232ace8f43fd80a291d9936
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-03-16 21:40:53 +0530

    Update player set11 4th.c

--- player set11 4th.c
+++ player set11 4th.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
-
-int main()
+int main(void) 
 {
-   int a[1000],num,i,sum=0;
-   scanf("%d",&num);
-   for(i=0;i<num;i++)
-   {
-       scanf("%d",&a[i]);
-   }
-   for(i=0;i<num;i++)
-   {
-       if(i!=num-1)
-       {
-       sum=sum+(a[i]+a[i+1]);
-       }
-   }
-   printf("%d",sum);
-    return 0;
+	int a[10],num,sum=0,i,j;
+	scanf("%d",&num);
+    for(i=0;i<num;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0,j=i+1;i<num,j<num;i++,j++)
+    {
+        sum=sum+a[i]+a[j];
+    }
+    printf("%d",sum);
+	return 0;
 }
