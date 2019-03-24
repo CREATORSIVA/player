22a39e31481d669399fdf4af28683e588c4cd990
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-03-24 18:21:46 +0530

    Create player set17 1st.c

---
+++ player set17 1st.c
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include<string.h>
+int main()
+{
+    char a[100][1000],b[1000];
+    int i,j,num,s,c=0;
+    scanf("%d",&num);
+    for(i=0;i<num;i++)
+    {
+        scanf("%s",a[i]);
+    }
+    for(i=0;i<num;i++)
+    {
+        strcpy(b,a[i]);
+        s=0;
+        for(j=0;b[j]!=NULL;j++)
+        {
+            if((b[j]=='a'||b[j]=='e'||b[j]=='i'||b[j]=='o'||b[j]=='u') || (b[j]=='A'||b[j]=='E'||b[j]=='I'||b[j]=='O'||b[j]=='U'))
+            {
+                s=1;
+            }
+        }
+        if(s==1)
+        {
+            c++;
+        }
+    }
+    if(c==num)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    return 0;
+}
