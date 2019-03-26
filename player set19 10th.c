b4eec3475641ebceb7e5ce6d2b2181bc1838a6aa
creatorsivasakthi <46025262+CREATORSIVA@users.noreply.github.com>
2019-03-26 07:44:16 +0530

    Create player set19 10th.c

---
+++ player set19 10th.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,x;
+    scanf("%d%d%d",&a,&b,&c);
+    x=(a*a)+(b*b)-(c*c);
+    if(x==0)
+        printf("yes");
+    else
+        printf("no");
+        return 0;
+}
